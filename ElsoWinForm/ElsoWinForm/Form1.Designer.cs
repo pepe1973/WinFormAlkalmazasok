edfdf5e49740b1d8218bdf4299c79d5b7d91ab4a
pepe1973 <bodrogipr@gmail.com>
2020-02-17 12:05:13 +0100

    ismét kommit

---
+++ ElsoWinForm/ElsoWinForm/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ElsoWinForm
+{
+    partial class MacskasUrlap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MacskasUrlap));
+            this.BeviteliMezo = new System.Windows.Forms.TextBox();
+            this.ModositoGomb = new System.Windows.Forms.Button();
+            this.KimenoSzoveg = new System.Windows.Forms.TextBox();
+            this.EltuntetGomb = new System.Windows.Forms.Button();
+            this.BezarGomb = new System.Windows.Forms.Button();
+            this.szoveg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BeviteliMezo
+            // 
+            this.BeviteliMezo.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.BeviteliMezo.Location = new System.Drawing.Point(228, 45);
+            this.BeviteliMezo.Multiline = true;
+            this.BeviteliMezo.Name = "BeviteliMezo";
+            this.BeviteliMezo.PasswordChar = 'ß';
+            this.BeviteliMezo.Size = new System.Drawing.Size(100, 20);
+            this.BeviteliMezo.TabIndex = 1;
+            this.BeviteliMezo.TextChanged += new System.EventHandler(this.BeviteliMezo_TextChanged);
+            // 
+            // ModositoGomb
+            // 
+            this.ModositoGomb.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ModositoGomb.Location = new System.Drawing.Point(37, 191);
+            this.ModositoGomb.Name = "ModositoGomb";
+            this.ModositoGomb.Size = new System.Drawing.Size(90, 55);
+            this.ModositoGomb.TabIndex = 2;
+            this.ModositoGomb.Text = "Módosítógomb";
+            this.ModositoGomb.UseVisualStyleBackColor = false;
+            this.ModositoGomb.Click += new System.EventHandler(this.ModositoGomb_Click);
+            this.ModositoGomb.MouseEnter += new System.EventHandler(this.ModositoGomb_MouseEnter);
+            this.ModositoGomb.MouseLeave += new System.EventHandler(this.ModositoGomb_MouseLeave);
+            // 
+            // KimenoSzoveg
+            // 
+            this.KimenoSzoveg.Location = new System.Drawing.Point(228, 89);
+            this.KimenoSzoveg.Name = "KimenoSzoveg";
+            this.KimenoSzoveg.Size = new System.Drawing.Size(100, 20);
+            this.KimenoSzoveg.TabIndex = 3;
+            // 
+            // EltuntetGomb
+            // 
+            this.EltuntetGomb.BackColor = System.Drawing.Color.Lime;
+            this.EltuntetGomb.Location = new System.Drawing.Point(293, 191);
+            this.EltuntetGomb.Name = "EltuntetGomb";
+            this.EltuntetGomb.Size = new System.Drawing.Size(90, 55);
+            this.EltuntetGomb.TabIndex = 4;
+            this.EltuntetGomb.Text = "Eltüntet";
+            this.EltuntetGomb.UseVisualStyleBackColor = false;
+            this.EltuntetGomb.Click += new System.EventHandler(this.EltuntetGomb_Click);
+            // 
+            // BezarGomb
+            // 
+            this.BezarGomb.BackColor = System.Drawing.Color.Yellow;
+            this.BezarGomb.Location = new System.Drawing.Point(531, 191);
+            this.BezarGomb.Name = "BezarGomb";
+            this.BezarGomb.Size = new System.Drawing.Size(90, 55);
+            this.BezarGomb.TabIndex = 5;
+            this.BezarGomb.Text = "Bezárás";
+            this.BezarGomb.UseVisualStyleBackColor = false;
+            this.BezarGomb.Click += new System.EventHandler(this.BezarGomb_Click);
+            this.BezarGomb.MouseEnter += new System.EventHandler(this.BezarGomb_MouseEnter);
+            // 
+            // szoveg
+            // 
+            this.szoveg.AutoSize = true;
+            this.szoveg.BackColor = System.Drawing.Color.White;
+            this.szoveg.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szoveg.ForeColor = System.Drawing.Color.DarkRed;
+            this.szoveg.Location = new System.Drawing.Point(74, 42);
+            this.szoveg.Name = "szoveg";
+            this.szoveg.Size = new System.Drawing.Size(70, 22);
+            this.szoveg.TabIndex = 0;
+            this.szoveg.Text = "Szöveg";
+            this.szoveg.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MacskasUrlap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(761, 335);
+            this.Controls.Add(this.BezarGomb);
+            this.Controls.Add(this.EltuntetGomb);
+            this.Controls.Add(this.KimenoSzoveg);
+            this.Controls.Add(this.ModositoGomb);
+            this.Controls.Add(this.BeviteliMezo);
+            this.Controls.Add(this.szoveg);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(300, 300);
+            this.Name = "MacskasUrlap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Macskás Űrlap";
+            this.Load += new System.EventHandler(this.MacskasUrlap_Load);
+            this.MouseEnter += new System.EventHandler(this.MacskasUrlap_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.MacskasUrlap_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MacskasUrlap_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox BeviteliMezo;
+        private System.Windows.Forms.Button ModositoGomb;
+        private System.Windows.Forms.TextBox KimenoSzoveg;
+        private System.Windows.Forms.Button EltuntetGomb;
+        private System.Windows.Forms.Button BezarGomb;
+        private System.Windows.Forms.Label szoveg;
+    }
+}
+
---
+++ ElsoWinForm/ElsoWinForm/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElsoWinForm
+{
+    public partial class MacskasUrlap : Form
+    {
+        public string belepoSzoveg = "Macskás űrlap";
+        public MacskasUrlap()
+        {
+            InitializeComponent();
+        }
+
+        private void ModositoGomb_Click(object sender, EventArgs e)
+        {
+            /*string modositandoSzoveg = BeviteliMezo.Text;
+            KimenoSzoveg.Text = modositandoSzoveg.ToUpper();*/
+            //this.Close();
+            //MessageBox.Show("Éljen a 13SZF", "Ez egy üdvözlő üzenet", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            /*if (EltuntetGomb.Visible)
+            {
+                EltuntetGomb.Visible = false;
+            }
+            else
+            {
+                EltuntetGomb.Visible = true;
+            }*/
+            EltuntetGomb.Visible = !EltuntetGomb.Visible;
+        }
+
+        private void MacskasUrlap_MouseEnter(object sender, EventArgs e)
+        {
+            this.Text = "Belépett";
+        }
+
+        private void MacskasUrlap_MouseLeave(object sender, EventArgs e)
+        {
+            this.Text = belepoSzoveg;
+        }
+
+        private void MacskasUrlap_Load(object sender, EventArgs e)
+        {
+            this.Text = belepoSzoveg.ToUpper();
+        }
+
+        private void MacskasUrlap_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = "Mozgás";
+        }
+
+        private void BeviteliMezo_TextChanged(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            int szam = rnd.Next(5);
+            switch (szam)
+            {
+                case 0:
+                    {
+                        this.BackColor = Color.Blue;
+                        break;
+                    }
+                case 1:
+                    {
+                        this.BackColor = Color.Beige;
+                        break;
+                    }
+                case 2:
+                    {
+                        this.BackColor = Color.Brown;
+                        break;
+                    }
+                case 3:
+                    {
+                        this.BackColor = Color.BlueViolet;
+                        break;
+                    }
+                case 4:
+                    {
+                        this.BackColor = Color.Chocolate;
+                        break;
+                    }
+            }
+            
+        }
+
+        private void ModositoGomb_MouseEnter(object sender, EventArgs e)
+        {
+            //ModositoGomb.Visible = false;
+        }
+
+        private void ModositoGomb_MouseLeave(object sender, EventArgs e)
+        {
+            //ModositoGomb.Visible = true;
+        }
+
+        private void EltuntetGomb_Click(object sender, EventArgs e)
+        {
+            if (ModositoGomb.Enabled)
+            {
+                ModositoGomb.Enabled = false;
+            }
+            else
+            {
+                ModositoGomb.Enabled = true;
+            }
+            //ModositoGomb.Enabled = !ModositoGomb.Enabled;
+        }
+
+        private void BezarGomb_Click(object sender, EventArgs e)
+        {
+            BezarGomb.Location = new Point(0, 0);
+        }
+
+        private void BezarGomb_MouseEnter(object sender, EventArgs e)
+        {
+            int szelesseg = this.Size.Width - BezarGomb.Width -10;
+            int magassag = this.Size.Height - BezarGomb.Height - 10;
+
+            Random rnd = new Random();
+
+            int x = rnd.Next(szelesseg);
+            int y = rnd.Next(magassag);
+            BezarGomb.Location = new Point(x, y);
+        }
+    }
+}
